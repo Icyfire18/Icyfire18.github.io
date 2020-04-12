9877428310d3c96148c6a9af9c11adee1e2c8d80
Icyfire18 <48960462+Icyfire18@users.noreply.github.com>
2020-04-13 00:39:26 +0530

    additional files required

---
+++ mail/contact_me.php
@@ -0,0 +1,25 @@
+<?php
+// Check for empty fields
+if(empty($_POST['name'])      ||
+   empty($_POST['email'])     ||
+   empty($_POST['phone'])     ||
+   empty($_POST['message'])   ||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+   echo "No arguments Provided!";
+   return false;
+   }
+   
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$email_address = strip_tags(htmlspecialchars($_POST['email']));
+$phone = strip_tags(htmlspecialchars($_POST['phone']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
+   
+// Create the email and send the message
+$to = "armaan.17cs@kct.ac.in"; // Add your email address inbetween the '' replacing urname@yourdomain.com This is where the form will send a message to.
+$email_subject = "Website Contact Form:  $name";
+$email_body = "You have received a new message from your website contact form.\n\n"."Here are the details:\n\nName: $name\n\nEmail: $email_address\n\nPhone: $phone\n\nMessage:\n$message";
+$headers = "From: noreply@icyfire.com\n"; // This is the email address the generated message will be from. We recommend using something like noreply@yourdomain.com.  
+mail('armaansait.as@gmail.com',$email_subject,$email_body,$headers);
+return true;         
+?>
